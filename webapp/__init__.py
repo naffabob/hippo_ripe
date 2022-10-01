d72fdf7b971361aa30907d2e8f65c29e7c24b3d9
naffabob <112754974+naffabob@users.noreply.github.com>
2022-10-02 00:43:35 +0300

    Bootstrap index.html created (#2)

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+
 from webapp.provider_ripedb import get_provider_clients_by_asset
 from webapp.settings import PROVIDER_ASSET
 
@@ -8,6 +9,7 @@ def create_app():
 
     @app.route('/')
     def index():
-        return get_provider_clients_by_asset()
+        title = 'Provider RIPE DB'
+        return render_template('index.html', page_title=title)
 
     return app
