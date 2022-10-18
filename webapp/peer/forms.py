2c794db966da9bb17d186e0833cbe03149efe667
Natalia Bobkova <naffa.bob@gmail.com>
2022-10-19 01:24:22 +0300

    Cosmetics with peer_form

--- webapp/peer/forms.py
+++ webapp/peer/forms.py
@@ -5,15 +5,20 @@ from wtforms.validators import DataRequired, Length, Regexp
 
 class PeerForm(FlaskForm):
     render_kw = {"class": "form-control"}
-
+    min_string_value = 5
+    max_string_value = 13
     asn = StringField(
         label='Номер AS',
         validators=[
-            DataRequired(message="Поле обязательно для заполнения"),
-            Length(min=5, max=13, message=f"Количество симовлов должно быть от 5 до 13"),
-            Regexp(regex='^AS[0-9]*$', message="Неверный формат AS-NUM"),
+            DataRequired(message='Поле обязательно для заполнения'),
+            Length(
+                min=min_string_value,
+                max=max_string_value,
+                message=f'Количество симовлов должно быть от {min_string_value} до {max_string_value}'
+            ),
+            Regexp(regex='^AS[0-9]*$', message='Неверный формат AS-NUM'),
         ],
-        render_kw={"class": "form-control"}
+        render_kw=render_kw
     )
     asset = StringField(label='AS-SET', render_kw=render_kw)
     remark = StringField(label='Описание', render_kw=render_kw)
