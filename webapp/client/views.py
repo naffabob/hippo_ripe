8bf2432271cf4beeb4b0f0481482699a281fbcc0
Natalia Bobkova <naffa.bob@gmail.com>
2022-10-22 14:21:11 +0300

    client views renamed

--- webapp/client/views.py
+++ webapp/client/views.py
@@ -8,14 +8,14 @@ blueprint = Blueprint('client', __name__, url_prefix='/clients')
 
 
 @blueprint.route('/')
-def clients():
+def clients_view():
     clients = Client.query.all()
     page = 'clients'
     return render_template('client/clients.html', clients=clients, page=page)
 
 
 @blueprint.route('/<int:client_id>', methods=['POST', 'GET'])
-def client(client_id):
+def client_view(client_id):
     client = Client.query.get(client_id)
     client_form = ClientForm(obj=client)
     if request.method == 'POST':
@@ -25,12 +25,12 @@ def client(client_id):
             db.session.add(client)
             db.session.commit()
             flash('Данные успешно сохранены')
-            return redirect(url_for('client.clients'))
+            return redirect(url_for('client.clients_view'))
     return render_template('client/client.html', form=client_form, client=client)
 
 
 @blueprint.route('/add', methods=['POST', 'GET'])
-def add_client():
+def add_client_view():
     client = Client()
     client_form = ClientForm()
     if request.method == 'POST':
@@ -39,5 +39,5 @@ def add_client():
             db.session.add(client)
             db.session.commit()
             flash('Данные успешно сохранены')
-            return redirect(url_for('client.clients'))
+            return redirect(url_for('client.clients_view'))
     return render_template('client/add_client.html', form=client_form, client=client)
