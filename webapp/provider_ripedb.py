bba8d2bbb84bec0e26e3fe233e539e11d2daf48b
Natalia Bobkova <naffa.bob@gmail.com>
2022-10-23 14:08:37 +0300

    change get_prefixes to get_prefixes_bgpq3

--- webapp/provider_ripedb.py
+++ webapp/provider_ripedb.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 
 from webapp.db import db
@@ -28,9 +30,29 @@ def get_prefixes(asn: str) -> list:
     return prefixes
 
 
-def save_prefixes(prefixes: list, peer: int):
-    for prefix in prefixes:
-        prefix_exist = Prefix.query.filter(Prefix.prefix == prefix).count()
-        if not prefix_exist:
-            db.session.add(Prefix(prefix=prefix, peer_id=peer, state='current'))
+def get_prefixes_bgpq3(ripe_obj) -> set:
+    stream = os.popen(f'bgpq3 -J -m 24 {ripe_obj}')
+    bgp_output = stream.read()
+    prefixes = {line.strip(' ;') for line in bgp_output.splitlines() if '/' in line}
+    return prefixes
+
+
+def save_prefixes(peer_id: int, ripe_prefixes: set):
+    db_prefixes = {x.prefix: x for x in Prefix.query.filter(Prefix.peer_id == peer_id)}
+
+    ripe_new = ripe_prefixes - db_prefixes.keys()
+
+    for prefix_obj in db_prefixes.values():
+        if prefix_obj.prefix in ripe_prefixes:
+            if prefix_obj.state == Prefix.STATE_TODELETE:
+                prefix_obj.state = Prefix.STATE_CURRENT
+                db.session.add(prefix_obj)
+        else:
+            prefix_obj.state = Prefix.STATE_TODELETE
+            db.session.add(prefix_obj)
+
+    for prefix in ripe_new:
+        prefix_obj = Prefix(prefix=prefix, state=Prefix.STATE_NEW, peer_id=peer_id)
+        db.session.add(prefix_obj)
+
     db.session.commit()
